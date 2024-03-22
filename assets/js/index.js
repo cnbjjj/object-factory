b5a78471950ab9e0adfb6af5e3950a321032e198
J <cnbjjj@outlook.com>
2024-03-22 18:18:57 -0500

    Change shapes storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,11 @@ import * as utils from './utils.js';
 import Shape from './Shape.js';
 
 function createShape() {
-    if (shapeDivs.children.length >= ROWS * COLS)
+    if (shapesArray.length >= ROWS * COLS)
         return shapeInfo.innerHTML = '<span class="warning">Container is full.</span>';
     if (shapes.selectedIndex === 0 || colors.selectedIndex === 0)
         return shapeInfo.innerHTML = '<span class="warning">Please select a shape and a color to create a shape.</span>';
-    const id = shapeDivs.children.length + 1;
+    const id = shapesArray.length + 1;
     const shapeClass = shapes.value;
     const shapeColor = { name: colors[colors.selectedIndex].text, value: colors.value };
     const shapeInst = new Shape(id, shapeClass, shapeColor);
@@ -17,12 +17,14 @@ function createShape() {
     utils.event(shapeDiv, 'click', () => shapeInfo.innerHTML = `Info ${shapeInst.getInfo()}`);
     shapeDivs.appendChild(shapeDiv);
     shapeInfo.innerHTML = `Created ${shapeInst.getInfo()}`;
+    shapesArray.push(shapeInst);
     return shapeInst;
 }
 
 const style = getComputedStyle(document.documentElement);
 const ROWS = style.getPropertyValue("--rows");
 const COLS = style.getPropertyValue("--cols");
+const shapesArray = new Array();
 const colors = utils.select('.colors');
 const shapes = utils.select('.shapes');
 const shapeDivs = utils.select('.shape-container');
